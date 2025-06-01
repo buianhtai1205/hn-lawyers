0212f1c60a22612649f84a3807ae456ef2568120
buianhtai1205 <buianhtai2017tq@gmail.com>
2025-06-01 11:10:47 +0000

    UI: fix UI services-section

--- hn-lawyers-ui/src/app/features/home/home.component.ts
+++ hn-lawyers-ui/src/app/features/home/home.component.ts
@@ -17,15 +17,15 @@ export class HomeComponent {
         {
             titleSidebar: 'TRỌNG TÀI & TỐ TỤNG TÒA ÁN',
             title: 'Trọng tài & Tố tụng Tòa án',
-            description: `Công ty Luật BLawyers Vietnam hỗ trợ nhiều vụ tố tụng Tòa án, tố tụng Trọng tài và đàm phán, thương lượng khác nhau. Chúng tôi cũng có kinh nghiệm trong vấn đề dân sự và thương mại nói chung, bao gồm thu hồi nợ và yêu cầu bồi thường thiệt hại. <br /> <br />
-                    Công ty Luật BLawyers Vietnam hỗ trợ nhiều vụ tố tụng Tòa án, tố tụng Trọng tài và đàm phán, thương lượng khác nhau. Chúng tôi cũng có kinh nghiệm trong vấn đề dân sự và thương mại nói chung, bao gồm thu hồi nợ và yêu cầu bồi thường thiệt hại.`,
+            description: `Công ty Luật HNLawyers Vietnam hỗ trợ nhiều vụ tố tụng Tòa án, tố tụng Trọng tài và đàm phán, thương lượng khác nhau. Chúng tôi cũng có kinh nghiệm trong vấn đề dân sự và thương mại nói chung, bao gồm thu hồi nợ và yêu cầu bồi thường thiệt hại. <br /> <br />
+                    Công ty Luật HNLawyers Vietnam hỗ trợ nhiều vụ tố tụng Tòa án, tố tụng Trọng tài và đàm phán, thương lượng khác nhau. Chúng tôi cũng có kinh nghiệm trong vấn đề dân sự và thương mại nói chung, bao gồm thu hồi nợ và yêu cầu bồi thường thiệt hại.`,
             moreText: 'MORE ABOUT TRỌNG TÀI & TỐ TỤNG',
             image: 'https://www.blawyersvn.com/wp-content/uploads/2020/06/arbitration-service.jpg',
         },
         {
             titleSidebar: 'DOANH NGHIỆP & ĐẦU TƯ NƯỚC NGOÀI',
             title: 'Doanh nghiệp & Đầu tư nước ngoài',
-            description: `Công ty Luật BLawyers Vietnam cung cấp một danh mục đầy đủ các dịch vụ cho khách hàng đầu tư tất cả các lĩnh vực trên khắp lãnh thổ Việt Nam, bao gồm công nghiệp (như là sản xuất và chế biến), nông nghiệp và nuôi trồng thủy sản và dịch vụ (như là vận tải, viễn thông, khách sạn, du lịch và cơ sở hạ tầng thể thao, ngân hàng và tài chính, văn hóa, y tế và giáo dục, khu dân cư, công trình thương mại và xây dựng kết cấu hạ tầng).`,
+            description: `Công ty Luật HNLawyers Vietnam cung cấp một danh mục đầy đủ các dịch vụ cho khách hàng đầu tư tất cả các lĩnh vực trên khắp lãnh thổ Việt Nam, bao gồm công nghiệp (như là sản xuất và chế biến), nông nghiệp và nuôi trồng thủy sản và dịch vụ (như là vận tải, viễn thông, khách sạn, du lịch và cơ sở hạ tầng thể thao, ngân hàng và tài chính, văn hóa, y tế và giáo dục, khu dân cư, công trình thương mại và xây dựng kết cấu hạ tầng).`,
             moreText: 'MORE ABOUT DOANH NGHIỆP & ĐẦU TƯ NƯỚC NGOÀI',
             image: 'https://www.blawyersvn.com/wp-content/uploads/2020/06/Corporate-Investment.jpg',
         },
@@ -40,14 +40,14 @@ export class HomeComponent {
         {
             titleSidebar: 'LAO ĐỘNG & VIỆC LÀM',
             title: 'Lao động & Việc làm',
-            description: `Công ty Luật BLawyers Vietnam có kinh nghiệm phong phú về việc tư vấn cho khách hàng doanh nghiệp về các vấn đề lao động và việc làm. Chúng tôi cũng có kinh nghiệm trong việc bảo vệ người lao động khi đại diện cho họ trong việc thương lượng với người sử dụng lao động của họ về vấn đề chấm dứt quan hệ lao động trái luật và trước tòa án giải quyết tranh chấp liên quan đến lao động.`,
+            description: `Công ty Luật HNLawyers Vietnam có kinh nghiệm phong phú về việc tư vấn cho khách hàng doanh nghiệp về các vấn đề lao động và việc làm. Chúng tôi cũng có kinh nghiệm trong việc bảo vệ người lao động khi đại diện cho họ trong việc thương lượng với người sử dụng lao động của họ về vấn đề chấm dứt quan hệ lao động trái luật và trước tòa án giải quyết tranh chấp liên quan đến lao động.`,
             moreText: 'MORE ABOUT LAO ĐỘNG',
             image: 'https://www.blawyersvn.com/wp-content/uploads/2020/06/Labor-Employment-1.jpg',
         },
         {
             titleSidebar: 'QUYỀN RIÊNG TƯ VỀ DỮ LIỆU',
             title: 'Quyền riêng tư về dữ liệu',
-            description: `BLawyers Vietnam có các luật sư chuyên nghiệp chuyên tư vấn các vấn đề bảo vệ dữ liệu, từ thu thập, xử lý, lưu trữ và chuyển giao thông tin cá nhân đến hỗ trợ khách hàng về các vấn đề tuân thủ quy định.`,
+            description: `HNLawyers Vietnam có các luật sư chuyên nghiệp chuyên tư vấn các vấn đề bảo vệ dữ liệu, từ thu thập, xử lý, lưu trữ và chuyển giao thông tin cá nhân đến hỗ trợ khách hàng về các vấn đề tuân thủ quy định.`,
             moreText: 'MORE ABOUT QUYỀN RIÊNG TƯ',
             image: 'https://www.blawyersvn.com/wp-content/uploads/2023/09/Du-lieu-ca-nhan-BLawyers-Vietnam.jpg',
         },
@@ -68,7 +68,7 @@ export class HomeComponent {
         {
             titleSidebar: 'QUYỀN SỞ HỮU TRÍ TUỆ',
             title: 'Quyền sở hữu trí tuệ',
-            description: `BLawyers Vietnam có một đội ngũ luật sư có kinh nghiệm về sáng chế, nhãn hiệu, thiết kế, có trình độ và kỹ năng xử lý toàn bộ các dịch vụ pháp lý liên quan đến sở hữu trí tuệ tại Việt Nam. Chúng tôi hỗ trợ khách hàng trong tất cả các bước của giai đoạn quản lý quyền sở hữu trí tuệ.`,
+            description: `HNLawyers Vietnam có một đội ngũ luật sư có kinh nghiệm về sáng chế, nhãn hiệu, thiết kế, có trình độ và kỹ năng xử lý toàn bộ các dịch vụ pháp lý liên quan đến sở hữu trí tuệ tại Việt Nam. Chúng tôi hỗ trợ khách hàng trong tất cả các bước của giai đoạn quản lý quyền sở hữu trí tuệ.`,
             moreText: 'MORE ABOUT SỞ HỮU TRÍ TUỆ',
             image: 'https://www.blawyersvn.com/wp-content/uploads/2022/09/Intellectual-property-rights-BLawyers-Vietnam.jpg',
         },
@@ -89,13 +89,13 @@ export class HomeComponent {
         {
             titleSidebar: 'LĨNH VỰC KHÁC',
             title: 'Lĩnh vực khác',
-            description: `Công ty Luật BLawyers Vietnam có kinh nghiệm phong phú trong các lĩnh vực sau: giáo dục, bảo hiểm, kinh doanh mỹ phẩm, tên miền, thương mại quốc tế, sở hữu trí tuệ, khoa học đời sống và chăm sóc sức khỏe, vận chuyển hàng hóa, thuế, viễn thông, truyền thông và công nghệ (TMT), Dịch vụ luật sư tư vấn thường xuyên (hàng tháng/ hàng năm).`,
+            description: `Công ty Luật HNLawyers Vietnam có kinh nghiệm phong phú trong các lĩnh vực sau: giáo dục, bảo hiểm, kinh doanh mỹ phẩm, tên miền, thương mại quốc tế, sở hữu trí tuệ, khoa học đời sống và chăm sóc sức khỏe, vận chuyển hàng hóa, thuế, viễn thông, truyền thông và công nghệ (TMT), Dịch vụ luật sư tư vấn thường xuyên (hàng tháng/ hàng năm).`,
             moreText: 'MORE ABOUT LĨNH VỰC KHÁC',
             image: 'https://www.blawyersvn.com/wp-content/uploads/2020/06/Others.jpg',
         },
     ];
 
-    selectedServiceIndex = 6;
+    selectedServiceIndex = 0;
 
     selectService(index: number) {
         this.selectedServiceIndex = index;
