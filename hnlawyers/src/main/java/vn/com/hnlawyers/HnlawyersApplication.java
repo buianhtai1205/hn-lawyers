9e2aff6e9db782c8e6ac568862802cc301f32d1b
buianhtai1205 <buianhtai2017tq@gmail.com>
2025-06-22 15:02:37 +0700

    Feat: Init source BE

---
+++ hnlawyers/src/main/java/vn/com/hnlawyers/HnlawyersApplication.java
@@ -0,0 +1,20 @@
+package vn.com.hnlawyers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The type Hnlawyers application.
+ */
+@SpringBootApplication
+public class HnlawyersApplication {
+
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(HnlawyersApplication.class, args);
+    }
+}
---
+++ hnlawyers/src/test/java/vn/com/hnlawyers/HnlawyersApplicationTests.java
@@ -0,0 +1,18 @@
+package vn.com.hnlawyers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * The type Hnlawyers application tests.
+ */
+@SpringBootTest
+class HnlawyersApplicationTests {
+
+    /**
+     * Context loads.
+     */
+    @Test
+    void contextLoads() {
+    }
+}
